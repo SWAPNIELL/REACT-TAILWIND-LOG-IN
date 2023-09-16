96d74ac273fd40b2e84748e87375414f8803ff99
SWAPNIL PATIL <swapnilpatil1012003@gmail.com>
2023-09-16 18:39:11 +0530

    Added one feature

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -3,37 +3,40 @@ import './style.css';
 
 function LoginPage() {
   return (
-
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <marquee>LOGIN/REGISTER</marquee>
-      <div className="bg-white p-4 rounded-lg shadow-md text-center">
-        <h2 className="text-2xl font-semibold">Login</h2>
+    <div className="bg-white p-4 rounded-lg shadow-md text-center">
+      <marquee className="text-blue-500 font-bold text-xl">LOGIN/REGISTER</marquee>
+      <h2 className="text-2xl font-semibold">Login</h2>
 
-        <div className="mt-4">
-          <input
-            type="email" placeholder="Email" className="w-full px-4 py-2 border rounded-md focus:outline-none"
-          />
-        </div>
-        <div className="mt-4">
-          <input
-            type="password" placeholder="Password" className="w-full px-4 py-2 border rounded-md focus:outline-none"
-          />
-        </div>
-        <button
-          id="email-login-button"
-          className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-4 w-full"
-        >
-          Login
-        </button>
-        <div className="mt-4">Or</div>
-        <button
-          id="google-login-button"
-          className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-4 w-full"
-        >
-          Login with Google
-        </button>
+      <div className="mt-4">
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full px-4 py-2 border rounded-md focus:outline-none"
+        />
       </div>
+      <div className="mt-4">
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full px-4 py-2 border rounded-md focus:outline-none"
+        />
+      </div>
+      <button
+        id="email-login-button"
+        className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-4 w-full"
+      >
+        Login
+      </button>
+      <div className="mt-4">Or</div>
+      <button
+        id="google-login-button"
+        className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-4 w-full"
+      >
+        Login with Google
+      </button>
     </div>
+  </div>
   );
 }
 
